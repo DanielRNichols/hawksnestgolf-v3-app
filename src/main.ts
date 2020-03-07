d6ea48bf9672322d9acc4ca09e861e7ce46a7944
DanielRNichols <danielrnichols63@gmail.com>
2020-03-07 09:48:39 -0600

    Initial commit including bootstrap and fa

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import {Aurelia} from 'aurelia-framework'
+import environment from './environment';
+import 'bootstrap'; // load bootstrap JavaScript
+
+export function configure(aurelia: Aurelia) {
+  aurelia.use
+    .standardConfiguration()
+    .feature('resources');
+
+  aurelia.use.developmentLogging(environment.debug ? 'debug' : 'warn');
+
+  if (environment.testing) {
+    aurelia.use.plugin('aurelia-testing');
+  }
+
+  aurelia.start().then(() => aurelia.setRoot());
+}
